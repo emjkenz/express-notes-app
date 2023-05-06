895ddce031b1e9e0e4663bf2fc4cb7502a2f5d9d
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-06 20:24:55 +1000

    Added a route to read the database

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,24 @@ const fs = require('fs');
 // Add support for serving assets
 app.use(express.static('public'));
 
-const database = "db/db.json"
+const databaseDir = "db/";
+const database = databaseDir+"db.json";
+
+// Read the notes from the database
+const readNotes = () => {
+    let data = [];
+
+    if (fs.existsSync(database)) {
+        // Read the file if it exists
+        const jsonData = fs.readFileSync(database);
+        data = JSON.parse(jsonData);
+    }
+    else {
+        fs.mkdirSync(databaseDir, { recursive: true });
+    }
+
+    return data;
+};
 
 // Show the notes homepage
 app.get('/', (req, res) => {
@@ -15,6 +32,12 @@ app.get('/', (req, res) => {
 // Show the notes editor
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html');
+});
+
+// Show json data from db.json
+app.get('/api/notes', (req, res) => {
+    const notes = readNotes();
+    res.json(notes);
 });
 
 app.listen(3000)
