8147dfe8a558f0cd9435690d1e1d89381dab6086
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-06 20:12:12 +1000

    Added routes to serve the html files

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// Add support for serving assets
+app.use(express.static('public'));
+
+const database = "db/db.json"
+
+// Show the notes homepage
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Show the notes editor
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/notes.html');
+});
 
 app.listen(3000)
